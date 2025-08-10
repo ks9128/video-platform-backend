85227c63fe7d0b5ca511bd7c4342420705d0c7d6
Khalid <mdkhalid57470@gmail.com>
2025-08-10 15:00:00 +0530

    worked on health and user routes

--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -11,7 +11,7 @@ import {
     updateUserAvatar,
     updateUserCoverImage,
     getWatchHistory
-} from "../controllers/user.controllers.js";
+} from "../controllers/user.controller.js";
 import { upload } from "../middlewares/multer.middleware.js"
 import {verifyJWT} from "../middlewares/auth.middlewares.js"
 
